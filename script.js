e770cdee21dde65e073b6353dec08348cb9a3b1a
Harshal Galande <112915773+Harshalx07@users.noreply.github.com>
2026-03-09 09:57:40 +0530

    Implement task completion toggle with localStorage

---
+++ script.js
@@ -0,0 +1,23 @@
+const nodes = document.querySelectorAll(".node")
+
+nodes.forEach(node => {
+
+const id = node.dataset.id
+
+if(localStorage.getItem(id) === "done"){
+node.classList.add("completed")
+}
+
+node.addEventListener("click",()=>{
+
+node.classList.toggle("completed")
+
+if(node.classList.contains("completed")){
+localStorage.setItem(id,"done")
+}else{
+localStorage.removeItem(id)
+}
+
+})
+
+})
